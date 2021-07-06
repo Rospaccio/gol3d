422c0a60a72f6b647a48f95fff380ad23553d271
Rospaccio <alberto.mercati@gmail.com>
2021-07-06 18:57:48 +0200

    WIP - Add / remove cubes in cells

--- src/lifeenv.js
+++ src/lifeenv.js
@@ -9,57 +9,60 @@ class LifeEnvironment {
         this.scene = lifeScene;
         this.cubeStep = cubeStep;
 
-        this.cubeMaterial = new THREE.MeshPhongMaterial({color: 0x6610f2});
+        this.cubeMaterial = new THREE.MeshPhongMaterial({ color: 0x6610f2 });
 
         this.cubes = [];
-        this.cells = []; // {i: i, j: j, cube: mesh}
+        this.cells = [[], []]
     }
 
-    toggleCellAt(point){
+    toggleCellAt(point) {
 
-        console.log("Point of cube: ", point);
-
-        if(point.z > DELTA_Z)
+        if (point.z > DELTA_Z)
             return;
 
         let cellOrdinalX = Math.floor(point.x / this.cubeStep);
         let cellOrdinalY = Math.floor(point.y / this.cubeStep);
-        
-        let cubePosition = new THREE.Vector3(
-            (this.cubeStep / 2) + (this.cubeStep * cellOrdinalX),
-            (this.cubeStep / 2) + (this.cubeStep * cellOrdinalY),
-            0);
-        
-        let geom = new THREE.BoxGeometry(this.cubeStep);
-        let cubeMesh = new THREE.Mesh(geom, this.cubeMaterial);
-        cubeMesh.position.set(cubePosition.x, cubePosition.y, cubePosition.z);
-        this.scene.add(cubeMesh);
 
-        let cell = {i: cellOrdinalX, j:cellOrdinalY, cube: cubeMesh};
-        this.setCellAt(cellOrdinalX, cellOrdinalY, cell);
+        var existing;
+        if (existing = this.cellAt(cellOrdinalX, cellOrdinalY)) {
+            this.resetCell(existing);
+        }
+        else {
+            let cubePosition = new THREE.Vector3(
+                (this.cubeStep / 2) + (this.cubeStep * cellOrdinalX),
+                (this.cubeStep / 2) + (this.cubeStep * cellOrdinalY),
+                0);
 
-        // this.cubes.push(cubeMesh);
-        // this.cells.push({i: cellOrdinalX, j: cellOrdinalY});
+            let geom = new THREE.BoxGeometry(this.cubeStep);
+            let cubeMesh = new THREE.Mesh(geom, this.cubeMaterial);
+            cubeMesh.position.set(cubePosition.x, cubePosition.y, cubePosition.z);
+            this.scene.add(cubeMesh);
+
+            let cell = { i: cellOrdinalX, j: cellOrdinalY, cube: cubeMesh };
+            this.setCellAt(cellOrdinalX, cellOrdinalY, cell);
+
+            // this.cubes.push(cubeMesh);
+            // this.cells.push({i: cellOrdinalX, j: cellOrdinalY});
+        }
     }
 
-    resetCellAt(i, j){
-        this.cells[i, j] = undefined;
+    resetCell(cell) {
+
+        this.scene.remove(cell.cube);
+        this.cells[cell.i][cell.j] = undefined;
     }
 
-    setCellAt(i, j, cell){
-        this.cells[i, j] = cell;
+    setCellAt(i, j, cell) {
+
+        if(!this.cells[i]){
+            this.cells[i] = [];
+        }
+
+        this.cells[i][j] = cell;
     }
 
-    cellAt(i, j){
-        return this.cells[i, j];
-    }
-
-    addCubeInCell(cell){
-
-    }
-
-    removeCubeFromCell(cell){
-
+    cellAt(i, j) {
+        return this.cells[i] && this.cells[i][j];
     }
 
 }
